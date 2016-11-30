17b6345d08ea2b4c24981de9a3ad5caa13ea4378
1Will <876957797@qq.com>
2016-11-30 19:24:53 +0800

    Signed-off-by: 1Will <876957797@qq.com>

---
+++ JavaStudy/HelloWorlddemo/src/JavaCore/JavaCore/SystemDemo.java
@@ -0,0 +1,10 @@
+package JavaCore;
+
+import java.util.Properties;
+
+public class SystemDemo {
+	public static void main(String []args){
+		Properties a=System.getProperties();
+		System.out.println(a);
+	}
+}
---
+++ JavaStudy/HelloWorlddemo/src/JavaCore/JavaCore/TestString.java
@@ -0,0 +1,13 @@
+package JavaCore;
+
+public class TestString {
+
+	public static void main(String[] args) {
+		String str="Good Morning Hello";
+		char a[]=str.toCharArray();
+		int a1=a[0];
+		System.out.println(a1);
+		System.out.println(a[a.length-1]);
+	}
+
+}
