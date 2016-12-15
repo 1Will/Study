acf9090e2d7faf135665993218028deb60ca304c
1Will <876957797@qq.com>
2016-12-15 19:24:39 +0800

    Signed-off-by: 1Will <876957797@qq.com>

--- JavaStudy/HelloWorlddemo/src/JavaCore/getUrl/URLTest.java
+++ JavaStudy/HelloWorlddemo/src/JavaCore/getUrl/URLTest.java
@@ -12,9 +12,14 @@ import java.net.HttpURLConnection;
 import java.net.PasswordAuthentication;
 import java.net.URL;
 import java.net.URLConnection;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Properties;
+
+import writeReaderDemo.changWrite;
  
 public class URLTest {
+	/*static String sss="String a1=\"contract_field:contract_code\";String a2=\"closingPrice\";String a3=\"lastTradeTime\";String a4=\"settlementPrice\";String a5=\"volumeTotal\";";*/
  // 一个public方法，返回字符串，错误则返回"error open url"
  public static String getContent(String strUrl) {
   try {
@@ -67,11 +72,12 @@ public class URLTest {
    while ((curLine = reader.readLine()) != null) {
    content = content + curLine+ "/r/n";
    }
-   System.out.println("content= " + content);
+ /*  System.out.println("content= " + content);
+   System.out.println(getContent(url));*/
    is.close();
-   System.out.println(getContent(url));
-   return getContent(url);
+   return content+getContent(url);
  }
+ 
  public static void writeFile(String str1) throws IOException{
 	File f=new File("D:" + File.separator + "get.html");
 	OutputStream output = new FileOutputStream(f, true);  //append为true，写入不覆盖
@@ -80,9 +86,81 @@ public class URLTest {
 	output.close();
  }
  
- public static void main(String args[]) throws IOException{
-	String url = "http://www.eex.com/data//view/data/detail/emission-eua-spot-v2/2014/04.07.json";
-	writeFile(getsrc(url)); 
+/* String url = "http://www.eex.com/data//view/data/detail/emission-eua-spot-v2/2014/04.07.json";*/
+ public static  String[] getword(String url) throws IOException{
+	String a1="contract_field:contract_code";
+	String a2="closingPrice";
+	String a3="lastTradeTime";
+	String a4="settlementPrice";
+	String a5="volumeTotal";
+	String b[] =new String[5];
+	String st[]=getsrc(url).split("\"");
+	/*String st[]=sss.split("\"");*/
+	for(int i=0;i<st.length;i++){
+		if(st[i].contains(a1)){
+			b[0]=st[i+2];
+		}
+		if(st[i].contains(a2)){
+			b[1]=st[i+1];
+		}
+		if(st[i].contains(a3)){
+			b[2]=st[i+2];
+		}
+		if(st[i].contains(a4)){
+			b[3]=st[i+1];
+		}
+		if(st[i].contains(a5)){
+			b[4]=st[i+1];
+		}
+	}
+	return b;
  }
  
+ public static String changword(String b[]){
+	 String str="<tr>";
+	 for(int i=0;i<b.length;i++){
+		 str=str+"<td>"+b[i]+"</td>";
+	 }
+	 str=str+"</tr>";
+	 return str;
+ }
+ 
+ public static String[] http(){
+	 String http[] =new String[2900] ;
+	 String ht="http://www.eex.com/data//view/data/detail/emission-eua-spot-v2/";
+	 String json=".json";
+	 String str="2016/12.15";
+	 Calendar c = Calendar.getInstance();
+		c.set(2009,00,18);
+		
+		for(int i=0;i<=3000;i++){
+		c.add(Calendar.DAY_OF_MONTH, 1);
+		SimpleDateFormat sm=new SimpleDateFormat("yyyy/MM.dd");
+		String date= sm.format(c.getTime());
+		http[i]=ht+date+json;
+		if(date.equals(str)){
+			/*System.out.println(i);*/
+			break;
+			}
+		}
+	 return http;
+ }
+ 
+ 
+ public static void main(String args[]) throws IOException{
+/*	String url[]= http();
+*	writeFile("<html><body><table border=\"1\">");
+*	writeFile("<tr><td>Name</td><td>Last Price</td><td>Last Time</td><td>Settl. Price</td><td>Vol.</td></tr>");
+	
+	for(int i=0;i<4;i++){
+	for(int i=0;i<url.length;i++){
+		if(getword(url[i])[1]==null){
+			continue;
+		}
+		writeFile(changword(getword(url[i])));
+	}
+	writeFile("</table></body></html>");*/
+	 System.out.println( getword("")[0]);
+ }
 }
+ 
