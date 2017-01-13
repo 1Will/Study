d7be2193f6f2f308c457f8db7dc4ebb4aacbecb4
1Will <876957797@qq.com>
2017-01-13 13:57:38 +0800

    Signed-off-by: 1Will <876957797@qq.com>

---
+++ JavaStudy/HelloWorlddemo/src/JavaCore/JavaCore/Testtt.java
@@ -0,0 +1,17 @@
+package JavaCore;
+
+public class Testtt {
+
+	public static void main(String[] args) {
+		int a=1 ,b=1;
+		Integer aa=1 ,bb=1;
+		float aaa= 1F,bbb=1F;
+		Float aaaa=1F,bbbb=1F;
+		System.out.println(a==b);
+		System.out.println(aa==bb);
+		System.out.println(aaa==bbb);
+		System.out.println(aaaa==bbbb);
+		System.out.println(aaaa.equals(bbbb));
+	}
+
+}
